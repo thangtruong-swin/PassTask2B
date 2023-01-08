55f4cb2dc58a239c1b9ae9c9d0b4bf181ed6c554
Thang Truong <thang.truong.swin@gmail.com>
2023-01-08 15:14:32 +1100

    completed get latest value from Detail Activity

---
+++ app/src/main/java/au/edu/swin/sdmd/passtask2a_wish_you_were_here/Location.kt
@@ -0,0 +1,12 @@
+package au.edu.swin.sdmd.passtask2a_wish_you_were_here
+
+import kotlinx.android.parcel.Parcelize
+import android.os.Parcelable
+
+@Parcelize
+data class Location(
+    var cardViewTitle:String,
+    var title: String, var city: String,
+    var date: String, var rating: Double,
+    var visited: Boolean = false
+    ): Parcelable
